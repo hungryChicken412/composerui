add3fe92ee42647a30d0e10078e407c9855dac89
Vatsal Shukla <vatsalshukla2001@gmail.com>
2026-07-07 15:33:33 +0530

    jmm

---
+++ types/react-syntax-highlighter.d.ts
@@ -0,0 +1,10 @@
+declare module 'react-syntax-highlighter';
+declare module 'react-syntax-highlighter/dist/cjs';
+declare module 'react-syntax-highlighter/dist/cjs/prism';
+declare module 'react-syntax-highlighter/dist/cjs/styles/prism';
+declare module 'react-syntax-highlighter/dist/esm';
+declare module 'react-syntax-highlighter/dist/esm/prism';
+declare module 'react-syntax-highlighter/dist/esm/styles/prism';
+
+// Fallback for any deep imports
+declare module 'react-syntax-highlighter/*';
